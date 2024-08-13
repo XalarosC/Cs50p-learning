884adf6a7194e34e0939ea065c147ac6d2958a2f
XalarosC <143029888+XalarosC@users.noreply.github.com>
2024-08-13 18:47:03 +0200

    Jar.py

---
+++ Jar.py
@@ -0,0 +1,43 @@
+
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError
+        self._capacity = capacity
+        self._size = 0
+
+
+
+    def __str__(self):
+        return "🍪" * self._size
+
+    def deposit(self, n):
+        if n > self._capacity or n + self._size > self._capacity:
+            raise ValueError
+
+        self._size += n
+
+    def withdraw(self, n):
+        if n > self._size:
+            raise ValueError
+        self._size -= n
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def size(self):
+        return self._size
+
+
+
+
+def main():
+    jar = Jar(4)
+    jar.deposit(3)
+    jar.withdraw(1 )
+    print(jar.capacity)
+    print(jar.size)
+if __name__ == "__main__":
+    main()
