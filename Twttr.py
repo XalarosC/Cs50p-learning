41649b64ffdecc2a0431e167410381550ae561a4
XalarosC <143029888+XalarosC@users.noreply.github.com>
2024-08-13 17:37:17 +0200

    Twttr.py

---
+++ Twttr.py
@@ -0,0 +1,5 @@
+input = input("Input: ")
+
+i = input.replace("a","").replace("A","").replace("e","").replace("E","").replace("i","").replace("I","").replace("o","").replace("O","").replace("u","").replace("U","")
+
+print("Output: ",i)
