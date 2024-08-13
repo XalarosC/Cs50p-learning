472b98f82b62c4c450cf5068bb1003b8d2670a2a
XalarosC <143029888+XalarosC@users.noreply.github.com>
2024-08-13 18:37:30 +0200

    Lowercase.py

---
+++ Lowercase.py
@@ -0,0 +1,8 @@
+# Ask the user for a sentence
+user_input = input("Type a sentence: ")
+
+# Convert the sentence to lowercase
+lowercase_input = user_input.lower()
+
+# Show the lowercase sentence
+print(lowercase_input)
